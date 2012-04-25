71cfc07f2073102a729fc81e59815556d3c37efb
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-04-25 16:57:19 -0500

    first plain simple version

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-	echo 'hello world';
+<!doctype html>
+<charset="utf-8">
+<title>@josedaniel startpage</title>
+<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+<body>
+	<ul class="level_0">
+		<li>
+			<h2 style="border-bottom-color:#B65051">Development</h2>
+			<ul class="level_1">
+				<li><a href="http://zambo.josepatenina.dev">Zambo [local]</a></li>
+				<li><a href="http://carlosprieto.net">Blog de Carlos Prieto</a></li>
+				<li><a href="http://zambodev.8manos.in">Zambo [dev]</a></li>
+				<li><a href="http://plazaencasa.com">Plaza en Casa [local]</a></li>
+				<li><a href="http://zambodev.8manos.in">Zambo [production]</a></li>
+				<li><a href="http://plazaencasa.josepaternina.dev">Plaza en Casa [production]</a></li>
+			</ul>
+		</li>
+
+		<li>
+			<h2 style="border-bottom-color:#00AEFF">Google Chrome Apps</h2>
+			<ul class="level_1">
+				<li><a href="https://chrome.google.com/webstore?utm_source=chrome-ntp-icon">Chrome Web Store</a></li>
+				<li><a href="http://gmail.com">Gmail</a></li>
+				<li><a href="http://do.com">Do.com</a></li>
+				<li><a href="https://www.thoughtbox.es">Thougthboxes</a></li>
+				<li><a href="http://chrome.angrybirds.com/">Angry Birds</a></li>
+			</ul>
+		</li>
+	</ul>
+</body>
