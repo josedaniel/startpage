2f99de00299145e3a833cc33b91cedf3f2299420
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-04-25 19:16:25 -0500

    github connection improvements

--- github.php
+++ github.php
@@ -1,1 +1,14 @@
-<?php 'git pull'; ?>
+<?php $output = shell_exec('git pull'); ?>
+<style>
+	body{
+		background-color: #1A1A1A;
+  		color:#fff;
+	}
+
+	pre{
+		padding:20px;
+	}
+</style>
+<body>
+	<pre><?= $output ?></pre>
+</body>
